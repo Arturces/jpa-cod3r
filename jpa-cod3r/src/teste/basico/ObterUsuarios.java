dd0d9669d4d598b3462033f9e5a1ab534163de72
Artur Cesar <arturcesar57@gmail.com>
2022-10-09 20:44:59 -0300

    Implementação do Obtendo mais de um elementos de dados da base atraves da query JPQL

---
+++ jpa-cod3r/src/teste/basico/ObterUsuarios.java
@@ -0,0 +1,35 @@
+package teste.basico;
+
+import modelo.basico.Usuario;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+public class ObterUsuarios {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence
+                .createEntityManagerFactory("jpa-cod3r");
+        EntityManager em = emf.createEntityManager();
+
+ /*       String jpql = "select u from Usuario u";
+        TypedQuery<Usuario> query = em.createQuery(jpql, Usuario.class);
+        query.setMaxResults(2);*/
+
+        List<Usuario> usuarios = em.createQuery("select u from Usuario u", Usuario.class)
+                .setMaxResults(2)
+                .getResultList();
+
+        for(Usuario usuario: usuarios){
+            System.out.println("ID: " + usuario.getId()
+                    + "E-mail: " + usuario.getEmail());
+        }
+
+        em.close();
+        emf.close();
+    }
+}
